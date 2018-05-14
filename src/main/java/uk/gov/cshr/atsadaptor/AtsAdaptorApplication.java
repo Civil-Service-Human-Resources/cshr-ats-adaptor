1cbaa0e37d554d0a6d30b3ce146ef8be273d40b5
seamus <seamus.singh@valtech.co.uk>
2018-05-14 13:02:51 +0100

    CSHRCP-1055 - Added scheduling of job for every 5 min from 1 min past hr

--- src/main/java/uk/gov/cshr/atsadaptor/AtsAdaptorApplication.java
+++ src/main/java/uk/gov/cshr/atsadaptor/AtsAdaptorApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
 @EnableCaching
+@EnableScheduling
 public class AtsAdaptorApplication {
     public static void main(String[] args) {
         SpringApplication.run(AtsAdaptorApplication.class, args);
